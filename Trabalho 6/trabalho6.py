efde2fb3185a7b890e5e267a6797a5cbe6cb1846
amandacasanova07 <161471622+amandacasanova07@users.noreply.github.com>
2024-03-19 09:58:16 -0300

    Add files via upload

---
+++ Trabalho 6/trabalho6.py
@@ -0,0 +1,33 @@
+# Variáveis para armazenar 4 números
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# Variável para armazenar uma frase
+frase = "Python é uma linguagem de programação poderosa e versátil."
+
+# Variável para armazenar uma palavra
+palavra = "programação"
+
+media = (numero1 + numero2 + numero3 + numero4) / 4
+
+quadrado_numero1 = numero1 ** 2
+
+dobro_numero2 = numero2 * 2
+
+quantidade_letras_palavra = len(palavra)
+
+quantidade_espacos_frase = frase.count(' ')
+
+primeiro_maior_que_segundo = numero1 > numero2
+
+maior_numero = max(numero1, numero2, numero3, numero4)
+
+print(media)
+print(quadrado_numero1)
+print(dobro_numero2)
+print(quantidade_letras_palavra)
+print(quantidade_espacos_frase)
+print(primeiro_maior_que_segundo)
+print(maior_numero)
